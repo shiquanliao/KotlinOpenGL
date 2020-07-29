9fd8a0f97dc343983f71ac816943dc8c7d5e709a
shiquanliao <liaosq@zhipu-inc.com>
2020-07-29 19:30:22 +0800

    Texture: 联系,Make sure only the happy face looks in the other/reverse direction by changing the fragment shader

--- app/src/main/assets/fshader.glsl
+++ app/src/main/assets/fshader.glsl
@@ -7,5 +7,5 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main(){
 //    fragmentColor = texture(texture1, TexCoord);
-    fragmentColor = mix(texture(texture1, TexCoord),texture(texture2, TexCoord), 0.2);
+    fragmentColor = mix(texture(texture1, TexCoord),texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2);
 }
